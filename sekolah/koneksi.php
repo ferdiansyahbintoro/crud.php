49ece39ece1e5bc813ea943e3ffdd26338e5d7be
ferdiansyahbintoro <145628068+ferdiansyahbintoro@users.noreply.github.com>
2023-10-04 00:17:07 -0700

    Add files via upload

---
+++ sekolah/koneksi.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<?php
+
+//deklasrasi variabel
+$db_host = "localhost";
+$db_user = "root";
+$db_pass = "";
+$db_name = "db_sekolah";    
+
+$connection = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+?>
+
+</body>
+</html>
